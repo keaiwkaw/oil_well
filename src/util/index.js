f46cb0494a47003ea160f2386d2f19bba7f6cc58
keaiwkaw <1990222329>
2022-11-01 22:30:29 +0800

    fix: 修复get参数问题

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,11 @@
 function queryGetParams(url, data) {
   const haveParams = url.indexOf('?') == -1 ? true : false;
   let params = [];
-  Object.keys(data).forEach(key => params.push(key + '=' + data[key]))
+  Object.keys(data).forEach(key => {
+    if (data[key]) {
+      params.push(key + '=' + data[key])
+    }
+  })
   const p = params.join('&')
   return decodeURIComponent(url +( haveParams ? "?" : "&") + p);
 }
